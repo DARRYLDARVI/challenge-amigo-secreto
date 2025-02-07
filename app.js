f7d0ea7cce83b6cd72e939834728841b8e71a721
DARRYLDARVI <davidfigueroa040726@gmail.com>
2025-02-06 18:31:32 -0600

    creacion funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo.length === 0) {
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(amigo);
+        limpiarCaja();
+    }
+    console.log(amigos);
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
