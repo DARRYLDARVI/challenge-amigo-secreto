f33601c739fbfc355b05f2ea637d89c1095e21ca
DARRYLDARVI <davidfigueroa040726@gmail.com>
2025-02-07 18:50:44 -0600

    Agrega Funcion SortearAmigo y mostrarLista

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
-
+let lista = [];
 
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
     if (amigo.length === 0) {
         alert('Por favor, inserte un nombre');
     } else {
-        amigos.push(amigo);
+        lista.push(amigo);
         limpiarCaja();
     }
-    console.log(amigos);
+    mostrarLista();
+    return;
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
+}
+
+function mostrarLista() {
+    let elementoHTML = document.getElementById('listaAmigos');
+    elementoHTML.innerHTML = "";
+    lista.forEach((persona) => {
+        elementoHTML.innerHTML += `<li>${persona}</li>`;
+    });
+}
+
+function sortearAmigo() {
+    let numeroMaximo = lista.length;
+    if (numeroMaximo > 1) {
+        let numeroGenerado = Math.floor(Math.random() * numeroMaximo);
+        let elementoHTML = document.getElementById('resultado');
+        elementoHTML.innerHTML = lista[numeroGenerado];
+    } else {
+        alert(`Ingrese ${(numeroMaximo === 0) ? 'los nombres a sortear' :
+            'otro nombre a sortear'}`);
+    }
 }
